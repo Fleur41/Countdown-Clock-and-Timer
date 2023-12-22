d97a5919fabd0322f215ba6cfd96adf5798886f4
Fleur41 <Muriithikings456@gmail.com>
2023-12-22 12:41:00 +0300

    Final Countdown

--- Count.py
+++ Count.py
@@ -8,4 +8,11 @@ def countdown(user_time):
         mins, secs = divmod(user_time, 60)
         timer = '{:02d}:{:02d}'.format(mins, secs)
         print(timer, end = '\r')
-        
+        time.sleep(1)
+        user_time -= 1
+    print('Lift off!')
+
+
+if __name__ == '__main__':
+    user_time = int(input('Enter a time in seconds: '))
+    countdown(user_time)
