7903a6932631ca43677d356cc12c21001d23f336
Fleur41 <Muriithikings456@gmail.com>
2023-12-22 12:33:48 +0300

    Countdown

--- Count.py
+++ Count.py
@@ -3,4 +3,9 @@
 
 import time
 
-def countdown(user_time)
+def countdown(user_time):
+    while user_time >= 0:
+        mins, secs = divmod(user_time, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end = '\r')
+        
